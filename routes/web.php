cfcc751c5eefa8cf1be19e87d32723f088c6cf6b
Ivan Aulia Rahman <ivanar6542@gmail.com>
2021-11-12 13:29:51 +0700

    Add app layout and navbar component

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('landing');
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
