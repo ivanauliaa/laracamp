f6587c9684654d70901e3cd09b2c0d7f884edbcf
Ivan Aulia Rahman <ivanar6542@gmail.com>
2021-11-16 13:43:26 +0700

    Add already registered camp checkout validation on landing page

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Camp extends Model
 {
@@ -14,4 +15,9 @@ class Camp extends Model
         'title',
         'price'
     ];
+
+    public function getIsRegisteredAttribute()
+    {
+        return Checkout::whereCampId($this->id)->whereUserId(Auth::user()->id)->exists();
+    }
 }
